f09272f92df064974f13c14574c069f4af52f718
jean <jean_0720@hotmail.com>
2024-01-21 19:26:16 -0500

    Inicializacion proyecto

---
+++ front/dragoncentury/app/src/main/java/com/example/dragoncentury/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.dragoncentury
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+}
+
+
+/*
+#ffd02c
+#f18413
+#e5261d
+#ad191c
+#eb5c73
+#1e1d38
+#c8d2d3
+#fffbdf
+ */
